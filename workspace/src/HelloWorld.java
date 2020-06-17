00835c6c52dee1e6f7b95f13bfd7ce7b8297c327
Jonas @ CoopX <jonas.blasas.lonnum@coop.no>
2020-06-17 15:45:47 +0200

    Initial commit

---
+++ workspace/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+        while(true) {
+            continue;
+        }
+        
+        // Unreachable atm
+        //System.out.println("Hello world program exited!");
+    }
+}
